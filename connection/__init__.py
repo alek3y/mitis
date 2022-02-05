015af1fc00caf7a5f7cf8787533ca28a2c4164d7
alek3y <alek3y@protonmail.com>
2022-02-05 15:33:39 +0100

    Add Packet class for communication packets

--- connection/__init__.py
+++ connection/__init__.py
@@ -1,9 +1,36 @@
+from enum import Enum
 import socket
+
+class Packet:
+	class Type(Enum):
+		HELLO = (0, type(None))
+		RAW = (1, bytes)
+		CHAT = (2, str)
+		AUDIO = (3, bytes)
+		VIDEO = (4, bytes)	# TODO: Numpy array?
+
+	def __init__(self, packet_type, content=None):
+		if packet_type.value[1] != type(content):
+			raise ValueError(f"expected data type {packet_type.value[1]}")
+
+		if packet_type == self.Type.CHAT:
+			self.data = content.encode("utf-8")
+		else:
+			self.data = content
+
+		self.type = packet_type
+
+	def pack(self):
+		packet_bytes = self.type.value[0].to_bytes(1, "little")
+		if self.data:
+			packet_bytes += self.data
+		return len(packet_bytes).to_bytes(4, "little") + packet_bytes
 
 class Connection:
 	def __init__(self, ip):
 		self.socket = socket.socket(type=socket.SOCK_DGRAM)
 		self.ip = ip
+		# TODO: What about listening mode?
 
 	def send(data):
 		length = len(data).to_bytes(4, "little")
