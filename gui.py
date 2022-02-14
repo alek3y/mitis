2df42a5ea83027288352caed27f214af1c0bce52
Gabritorre <gabritorredev@gmail.com>
2022-02-14 20:06:32 +0100

    Add icons

--- gui.py
+++ gui.py
@@ -1,32 +1,58 @@
 # https://www.pythontutorial.net/tkinter/tkinter-hello-world/
-#temi: breeze-dark, yaru
 # pip3 install ttkthemes
-# temi: https://ttkthemes.readthedocs.io/en/latest/example.html
 # keybinds: https://www.pythontutorial.net/tkinter/tkinter-event-binding/
-
 #TODO fixare bottone che si preme solo in focus
+
 import tkinter as tk
 from tkinter import ttk
 from ttkthemes import ThemedTk
-import time
 
-FONT = ("Monospace", 16)
+FONT = ("Monospace", 11)
 PLACEHOLDER = "Inserisci un messaggio... "
 
-def hello(event, stringa = "ciao"):
-	global mic_state
+def micToggle(event, input_sorce = "none"):
+	global mic_status
 	message = ttk.Label(root, font=FONT)
-	print(stringa)	#debug
+	print(input_sorce)	#debug
 	print(event)	#debug
-	if(mic_state):	#se il microfono è attivo
+	if(mic_status):	#se il microfono è attivo
 		mute_button.config(image = mic_off)	#cambia l'immagine
-		mic_state = False	#setta il microfono come disattivo
-		message.config(text = "muted")
+		mic_status = False	#setta il microfono come disattivo
+		message.config(text = "listening no ")
 	else:
 		mute_button.config(image = mic_on)
-		mic_state = True
-		message.config(text = "unmuted")
+		mic_status = True
+		message.config(text = "listening yes")
 	message.grid(column = 1, row = 1, sticky = "s")
+
+def camToggle(event, input_sorce = "none"):
+	global cam_status
+	message = ttk.Label(root, font=FONT)
+	print(input_sorce)	#debug
+	print(event)	#debug
+	if(cam_status):	#se la cam è attiva
+		cam_button.config(image = cam_off)	#cambia l'immagine
+		cam_status = False	#imposta la cam come disattivata
+		message.config(text = "watching no ")
+	else:
+		cam_button.config(image = cam_on)
+		cam_status = True
+		message.config(text = "watching yes")
+	message.grid(column = 1, row = 1, sticky = "sw")
+
+def subtitlesToggle(event):
+	global subtitles_status
+	message = ttk.Label(root, font=FONT)
+	print(event)	#debug
+	if(subtitles_status):
+		subtitles_button.config(image = subtitles_off)
+		subtitles_status = False
+		message.config(text = "subtitles no ")
+	else:
+		subtitles_button.config(image = subtitles_on)
+		subtitles_status = True
+		message.config(text = "subtitles yes")
+	message.grid(column = 0, row = 0, sticky = "se")
 
 def clearTextbox(event):
 	global textbox, delete_text, text
@@ -49,13 +75,12 @@ def addPlaceholder(event):
 if __name__ == "__main__":
 	root = ThemedTk(theme = "breeze")	#finestra padre con tema breeze
 	root.title("Mitis")
-
 	width_screen = root.winfo_screenwidth()	# larghezza dello schermo
 	heigth_screen = root.winfo_screenheight() # altezza dello schermo
 	screen_size = str(width_screen) + "x" + str(heigth_screen)
-
 	root.geometry(screen_size)
 	root.minsize(800, 500)	#grandezza minima finestra
+
 	#creazione delle colonne e delle righe
 	root.columnconfigure(0, weight = 1)
 	root.columnconfigure(1, weight = 15)
@@ -63,46 +88,59 @@ if __name__ == "__main__":
 	root.rowconfigure(0, weight = 10)
 	root.rowconfigure(1, weight = 1)
 
-
-	mic_off = tk.PhotoImage(file = "assets/mute.png")
-	mic_on = tk.PhotoImage(file = "assets/unmute.png")
-	mic_state = False
+	#creazione delle icone
+	mic_off = tk.PhotoImage(file = "assets/mic_off.png")
+	mic_on = tk.PhotoImage(file = "assets/mic.png")
+	cam_off = tk.PhotoImage(file = "assets/cam_off.png")
+	cam_on = tk.PhotoImage(file = "assets/cam.png")
+	send = tk.PhotoImage(file = "assets/send_message.png")
+	filters = tk.PhotoImage(file = "assets/filters.png")
+	# mask = tk.PhotoImage(file = "assets/mask.png")	#TODO
+	subtitles_off = tk.PhotoImage(file = "assets/subtitles_off.png")
+	subtitles_on = tk.PhotoImage(file = "assets/subtitles.png")
 
 	communication_tools = ttk.Frame(root)	#frame che contiene i bottoni per il mute e lo spegnimento della cam
-
-	cam_button = ttk.Button(communication_tools, text = "cam on/off")
-	mute_button = ttk.Button(communication_tools, image = mic_off)
-	mute_button.bind("<Control-m>", lambda event:hello(event, "il microfono è stato mutato dalla shortcut"))	#shortcut: ctrl + m
-	mute_button.bind("<ButtonRelease>", lambda event:hello(event, "il microfono è stato mutato dal mouse"))	#chiama la funzione anche con il click del mouse quando rilasciato
-	mute_button.focus()	#rende il bottone sotto focus di default (per far funzionare la shortcut il bottone deve essere in focus)
 	
+	mic_status = False
+	mute_button = ttk.Button(communication_tools, image = mic_off)
+	mute_button.bind("<Control-m>", lambda event:micToggle(event, "il microfono è stato mutato dalla shortcut"))	#shortcut: ctrl + m
+	mute_button.bind("<ButtonRelease>", lambda event:micToggle(event, "il microfono è stato mutato dal mouse"))	#chiama la funzione anche con il click del mouse quando rilasciato
+	mute_button.focus()	#rende il bottone sotto focus di default (per far funzionare la shortcut il bottone deve essere in focus)
 	mute_button.pack(side="right", padx = 5)
+
+	cam_status = False
+	cam_button = ttk.Button(communication_tools, image = cam_off)
+	cam_button.bind("<Control-w>", lambda event:camToggle(event, "la webcam è stata mutata dalla shortcut"))	#shortcut: ctrl + m
+	cam_button.bind("<ButtonRelease>", lambda event:camToggle(event, "la webcam è stata mutata dal mouse"))	#chiama la funzione anche con il click del mouse quando rilasciato
 	cam_button.pack()
 
 	communication_tools.grid(column = 1, row = 1, sticky="n")
 
 	send_message = ttk.Frame(root)
 	text = tk.StringVar()
-	textbox = ttk.Entry(send_message, textvariable=text, width = 50)
+	textbox = ttk.Entry(send_message, textvariable=text, width = 45, font = FONT)
 	textbox.insert(0, PLACEHOLDER)	# inserire del testo che fungerà da placeholder #!approfondire
 	textbox.bind("<FocusIn>", lambda event:clearTextbox(event))
 	textbox.bind("<FocusOut>", lambda event:addPlaceholder(event))
-	textbox.pack(side = "left")
+	textbox.pack(side = "left", ipady = 3)
 
-	test_button = ttk.Button(send_message, text = "invia")
+	test_button = ttk.Button(send_message, image = send)
 	test_button.bind("<ButtonRelease>",lambda event : sendMessage(event))
 	test_button.pack(padx = 5, pady = 1)
 	send_message.grid(column = 2, row = 1, sticky = "ne")
 
 	tools = ttk.Frame(root)	#frame che contiene tutti i tools
 
-	filters_button= ttk.Button(tools, text = "filtri")
+	subtitles_status = False
+	filters_button= ttk.Button(tools, image = filters)
 	mask_button= ttk.Button(tools, text = "maschere")
-	freeze_button= ttk.Button(tools, text = "freeze")
+	subtitles_button= ttk.Button(tools, image = subtitles_off)
+	subtitles_button.bind("<ButtonRelease>", lambda event: subtitlesToggle(event))
 
 	filters_button.pack(side = "top", pady = 10)
 	mask_button.pack(side = "top")
-	freeze_button.pack(side = "top", pady = 10)
+	subtitles_button.pack(side = "top", pady = 10)
+	
 
 	tools.grid(column = 0, row = 0, sticky = "e")
 
