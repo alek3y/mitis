0a9eb30a242ad395a6b64af5283765b688d1ed26
Gabritorre <gabritorredev@gmail.com>
2022-02-16 18:02:33 +0100

    Fix class variables

--- gui/__init__.py
+++ gui/__init__.py
@@ -17,10 +17,10 @@ class Gui:
 	def __init__(self):
 		self.root = ThemedTk(theme = "breeze")	#finestra padre con tema breeze
 		self.root.title("Mitis")
-		self.width_screen = self.root.winfo_screenwidth()	# larghezza dello schermo
-		self.heigth_screen = self.root.winfo_screenheight() # altezza dello schermo
-		self.screen_size = str(self.width_screen) + "x" + str(self.heigth_screen)
-		self.root.geometry(self.screen_size)
+		width_screen = self.root.winfo_screenwidth()	# larghezza dello schermo
+		heigth_screen = self.root.winfo_screenheight() # altezza dello schermo
+		screen_size = str(width_screen) + "x" + str(heigth_screen)
+		self.root.geometry(screen_size)
 		self.root.minsize(800, 500)	#grandezza minima finestra
 
 		#creazione delle colonne e delle righe
@@ -31,57 +31,57 @@ class Gui:
 		self.root.rowconfigure(1, weight = 1)
 
 		#creazione delle icone
-		self.mic_off = tk.PhotoImage(file = "assets/mic_off.png")
-		self.mic_on = tk.PhotoImage(file = "assets/mic.png")
-		self.cam_off = tk.PhotoImage(file = "assets/cam_off.png")
-		self.cam_on = tk.PhotoImage(file = "assets/cam.png")
+		mic_off = tk.PhotoImage(file = "assets/mic_off.png")
+		mic_on = tk.PhotoImage(file = "assets/mic.png")
+		cam_off = tk.PhotoImage(file = "assets/cam_off.png")
+		cam_on = tk.PhotoImage(file = "assets/cam.png")
 		self.send = tk.PhotoImage(file = "assets/send_message.png")
 		self.filters = tk.PhotoImage(file = "assets/filters.png")
 		self.mask = tk.PhotoImage(file = "assets/mask.png")
-		self.subtitles_off = tk.PhotoImage(file = "assets/subtitles_off.png")
-		self.subtitles_on = tk.PhotoImage(file = "assets/subtitles.png")
+		subtitles_off = tk.PhotoImage(file = "assets/subtitles_off.png")
+		subtitles_on = tk.PhotoImage(file = "assets/subtitles.png")
 
-		self.communication_tools = ttk.Frame(self.root)	#frame che contiene i bottoni per il mute e lo spegnimento della cam
+		communication_tools = ttk.Frame(self.root)	#frame che contiene i bottoni per il mute e lo spegnimento della cam
 		
-		self.mute_button = ttk.Button(self.communication_tools, image = self.mic_off)
-		self.mute_button.bind("<Control-m>", lambda event:self.micToggle(event, self.mute_button, self.mic_off, self.mic_on, "il microfono è stato mutato dalla shortcut"))	#shortcut: ctrl + m
-		self.mute_button.bind("<ButtonRelease>", lambda event:self.micToggle(event,self.mute_button, self.mic_off, self.mic_on, "il microfono è stato mutato dal mouse"))	#chiama la funzione anche con il click del mouse quando rilasciato
-		self.mute_button.focus()	#rende il bottone sotto focus di default (per far funzionare la shortcut il bottone deve essere in focus)
-		self.mute_button.pack(side = "right", padx = 5)
+		mute_button = ttk.Button(communication_tools, image = mic_off)
+		mute_button.bind("<Control-m>", lambda event:self.micToggle(event, mute_button, mic_off, mic_on, "il microfono è stato mutato dalla shortcut"))	#shortcut: ctrl + m
+		mute_button.bind("<ButtonRelease>", lambda event:self.micToggle(event, mute_button, mic_off, mic_on, "il microfono è stato mutato dal mouse"))	#chiama la funzione anche con il click del mouse quando rilasciato
+		mute_button.focus()	#rende il bottone sotto focus di default (per far funzionare la shortcut il bottone deve essere in focus)
+		mute_button.pack(side = "right", padx = 5)
 
-		self.cam_button = ttk.Button(self.communication_tools, image = self.cam_off)
-		self.cam_button.bind("<Control-w>", lambda event:self.camToggle(event, self.cam_button, self.cam_off, self.cam_on, "la webcam è stata mutata dalla shortcut"))	#shortcut: ctrl + m
-		self.cam_button.bind("<ButtonRelease>", lambda event:self.camToggle(event, self.cam_button, self.cam_off, self.cam_on, "la webcam è stata mutata dal mouse"))	#chiama la funzione anche con il click del mouse quando rilasciato
-		self.cam_button.pack()
+		cam_button = ttk.Button(communication_tools, image = cam_off)
+		cam_button.bind("<Control-w>", lambda event:self.camToggle(event, cam_button, cam_off, cam_on, "la webcam è stata mutata dalla shortcut"))	#shortcut: ctrl + m
+		cam_button.bind("<ButtonRelease>", lambda event:self.camToggle(event, cam_button, cam_off, cam_on, "la webcam è stata mutata dal mouse"))	#chiama la funzione anche con il click del mouse quando rilasciato
+		cam_button.pack()
 
-		self.communication_tools.grid(column = 1, row = 1, sticky = "n")
+		communication_tools.grid(column = 1, row = 1, sticky = "n")
 
-		self.send_message_wrapper = ttk.Frame(self.root)
-		self.text = tk.StringVar()
-		self.textbox = ttk.Entry(self.send_message_wrapper, textvariable = self.text, width = 45, font = FONT)
-		self.textbox.insert(0, PLACEHOLDER)	# inserire del testo che fungerà da placeholder
-		self.textbox.bind("<FocusIn>", lambda event:self.clearTextbox(event, self.textbox, self.text))
-		self.textbox.bind("<FocusOut>", lambda event:self.addPlaceholder(event, self.textbox, self.text))
-		self.textbox.pack(side = "left", ipady = 3)
+		send_message_wrapper = ttk.Frame(self.root)
+		text = tk.StringVar()
+		textbox = ttk.Entry(send_message_wrapper, textvariable = text, width = 45, font = FONT)
+		textbox.insert(0, PLACEHOLDER)	# inserire del testo che fungerà da placeholder
+		textbox.bind("<FocusIn>", lambda event:self.clearTextbox(event, textbox, text))
+		textbox.bind("<FocusOut>", lambda event:self.addPlaceholder(event, textbox, text))
+		textbox.pack(side = "left", ipady = 3)
 
-		self.send_button = ttk.Button(self.send_message_wrapper, image = self.send)
-		self.send_button.bind("<ButtonRelease>",lambda event:self.sendMessage(event, self.textbox, self.text))
-		self.send_button.pack(padx = 5, pady = 1)
-		self.send_message_wrapper.grid(column = 2, row = 1, sticky = "ne")
+		send_button = ttk.Button(send_message_wrapper, image = self.send)
+		send_button.bind("<ButtonRelease>", lambda event:self.sendMessage(event, textbox, text))
+		send_button.pack(padx = 5, pady = 1)
+		send_message_wrapper.grid(column = 2, row = 1, sticky = "ne")
 
-		self.tools = ttk.Frame(self.root)	#frame che contiene tutti i tools
+		tools = ttk.Frame(self.root)	#frame che contiene tutti i tools
 
-		self.filters_button= ttk.Button(self.tools, image = self.filters)
-		self.mask_button= ttk.Button(self.tools, image = self.mask)
-		self.subtitles_button= ttk.Button(self.tools, image = self.subtitles_off)
-		self.subtitles_button.bind("<ButtonRelease>", lambda event:self.subtitlesToggle(event, self.subtitles_button, self.subtitles_off, self.subtitles_on))
+		filters_button= ttk.Button(tools, image = self.filters)
+		mask_button= ttk.Button(tools, image = self.mask)
+		subtitles_button= ttk.Button(tools, image = subtitles_off)
+		subtitles_button.bind("<ButtonRelease>", lambda event:self.subtitlesToggle(event, subtitles_button, subtitles_off, subtitles_on))
 
-		self.filters_button.pack(side = "top", pady = 10)
-		self.mask_button.pack(side = "top")
-		self.subtitles_button.pack(side = "top", pady = 10)
+		filters_button.pack(side = "top", pady = 10)
+		mask_button.pack(side = "top")
+		subtitles_button.pack(side = "top", pady = 10)
 		
 
-		self.tools.grid(column = 0, row = 0)
+		tools.grid(column = 0, row = 0)
 
 		# ttk.Label(self.root, text = "tools", background = "green", foreground = "white").grid(column = 0, row = 0, rowspan = 2, sticky="ewns")
 		ttk.Label(self.root, text = "cam zone", background = "red", foreground = "white").grid(column = 1, row = 0, sticky="ewns")
@@ -93,7 +93,7 @@ class Gui:
 
 	def micToggle(self, event, mute_button, mic_off, mic_on, input_source = "none"):
 		global mic_status
-		message = ttk.Label(self.root, font=FONT)
+		message = ttk.Label(self.root, font = FONT)
 		print(input_source)	#debug
 		print(event)	#debug
 		if(mic_status):	#se il microfono è attivo
