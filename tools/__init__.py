c0ffbe4ae4364700467b75dd02f52f357b8443d1
M3nny <77993658+M3nny@users.noreply.github.com>
2022-02-24 19:36:43 +0100

    Add Mask class

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,5 +1,5 @@
-import numpy as np
 import cv2
+import cvzone
 
 
 class Filter:
@@ -17,4 +17,26 @@ class Filter:
         elif filter == "mirror": 
             frame_flip = cv2.flip(frame,1)
             return np.hstack([frame,frame_flip])
+
+class Mask:
+    def __init__(self, faces_cascade):
+        self.faces_cascade = cv2.CascadeClassifier(faces_cascade)
+       
+    def face_mask(self, frame, overlay_image = "./masks/pusheen.png"):
+        overlay_image = cv2.imread(overlay_image, cv2.IMREAD_UNCHANGED)
+        gray_scale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = self.faces_cascade.detectMultiScale(gray_scale, 1.1, 4)
+        for (x, y, w, h) in faces:
+            overlay_resize = cv2.resize(overlay_image, (w, h))
+            frame = cvzone.overlayPNG(frame, overlay_resize, [x,y])
+        return frame
+
+    def glasses(self, frame, overlay_image = "./masks/mlg_glasses.png"):
+        overlay_image = cv2.imread(overlay_image, cv2.IMREAD_UNCHANGED)
+        gray_scale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = self.faces_cascade.detectMultiScale(gray_scale, 1.1, 4)
+        for (x, y, w, h) in faces:
+            overlay_resize = cv2.resize(overlay_image, (w, h))
+            frame = cvzone.overlayPNG(frame, overlay_resize, [x,y-5])
+        return frame
         
